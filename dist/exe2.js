fe7754f308b5dd26dd3629b557327b25181fc185
malamadre211 <mariangelacurcioartist@gmail.com>
2023-08-31 10:52:13 +0200

    Exercise 2

---
+++ dist/exe2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Exercice 2 - Les tableaux
+let tableau = [];
+let index = 0;
+while (index < 10) {
+    tableau[index] = index++;
+    index = index + 1;
+}
---
+++ src/exe2.ts
@@ -0,0 +1,8 @@
+// Exercice 2 - Les tableaux
+
+let tableau : number[] = [];
+let index = 0;
+while (index < 10) {
+    tableau[index] = index ++;
+    index = index + 1;
+}
