c99f0346a8988b7953751d1633a8c46c0565248f
malamadre211 <mariangelacurcioartist@gmail.com>
2023-09-01 12:04:58 +0200

    Commit 0109

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@
 import {decouverte_typescript_exe1} from './exe1';
 import {decouverte_typescript_exe2} from './exe2';
 import {decouverte_typescript_exe3} from './exe3';
-import {decouverte_typescript_FizzBuzz} from './FizzBuzz';
 
 // decouverte_typescript_exe3();
 
 // decouverte_typescript_exe1();
 
 // decouverte_typescript_exe2();
-
-decouverte_typescript_FizzBuzz();
